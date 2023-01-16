32a0e852ac530209b91fb454029d37b943318d0d
Tarek-Bohdima <tarek.buhdeima@gmail.com>
2023-01-17 00:14:37 +0200

    add feat authentication on backend

--- backend/src/util/validateEnv.ts
+++ backend/src/util/validateEnv.ts
@@ -1,7 +1,8 @@
-import { cleanEnv} from "envalid";
+import { cleanEnv } from "envalid";
 import { port, str } from "envalid/dist/validators";
 
 export default cleanEnv(process.env, {
     MONGO_CONNECTION_STRING: str(),
-    PORT: port()
+    PORT: port(),
+    SESSION_SECRET: str(),
 });
