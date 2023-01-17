bba1ad4845efe54728b1f4df632016e1cee6eb82
Tarek-Bohdima <tarek.buhdeima@gmail.com>
2023-01-18 00:28:00 +0200

    make authentication front end make navbar

--- frontend/src/components/AddEditNoteDialog.tsx
+++ frontend/src/components/AddEditNoteDialog.tsx
@@ -3,6 +3,7 @@ import { Note } from "../models/note";
 import { useForm } from "react-hook-form";
 import { NoteInput } from "../network/notes_api";
 import * as NotesApi from "../network/notes_api"
+import TextInputField from "./form/TextInputFields";
 
 interface AddEditNoteDialogProps {
     noteToEdit?: Note,
@@ -45,28 +46,25 @@ const AddEditNoteDialog = ({ noteToEdit, onDismiss, onNoteSaved }: AddEditNoteDi
 
             <Modal.Body>
                 <Form id="addEditNoteForm" onSubmit={handleSubmit(onSubmit)}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Title"
-                            isInvalid={!!errors.title}
-                            {...register("title", { required: "Required" })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.title?.message}
-                        </Form.Control.Feedback>
-                    </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Text</Form.Label>
-                        <Form.Control
-                            as="textarea"
-                            rows={5}
-                            placeholder="Text"
-                            {...register("text")}
-                        />
-                    </Form.Group>
+                    <TextInputField
+                        name="title"
+                        label="Title"
+                        type="text"
+                        placeholder="Title"
+                        register={register}
+                        registerOptions={{ required: "Required" }}
+                        error={errors.title}
+                    />
+
+                    <TextInputField
+                        name="text"
+                        label="Text"
+                        as="textarea"
+                        rows={5}
+                        placeholder="Text"
+                        register={register}
+                    />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
---
+++ frontend/src/components/NotesPageLoggedOutView.tsx
@@ -0,0 +1,7 @@
+const NotesPageLoggedOutView = () => {
+    return (
+        <p>Please log in to see your notes</p>
+    );
+}
+
+export default NotesPageLoggedOutView;
